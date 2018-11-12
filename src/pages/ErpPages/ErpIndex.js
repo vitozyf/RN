3ea18bf3ef2298cca86a8a277c0f0db5534301e4
vito <vitozhangyf@outlook.com>
2018-11-12 23:33:18 +0800

    格式化配置

--- src/pages/ErpPages/ErpIndex.js
+++ src/pages/ErpPages/ErpIndex.js
@@ -1,125 +1,146 @@
-import React, {Component} from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import {connect} from 'react-redux';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Feather from 'react-native-vector-icons/Feather';
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Feather from "react-native-vector-icons/Feather";
 // import Icon from '@components/Iconfont/CloudIcon'
 
-
-import {
-  HeaderTitle,
-  HeaderRight,
-} from '@components';
+import { HeaderTitle, HeaderRight } from "@components";
 class HeaderLeft extends Component {
   onPress = () => {
-    const {navigation} = this.props;
+    const { navigation } = this.props;
     navigation.openDrawer();
-  }
+  };
   render() {
     const { AvatarPath } = this.props;
     return (
-      <TouchableOpacity
-      activeOpacity={0.8}
-      onPress={ this.onPress }>
-        <Image 
-        style={styles.headerLeftImg}
-        source={{
-          uri: AvatarPath
-        }} />
+      <TouchableOpacity activeOpacity={0.8} onPress={this.onPress}>
+        <Image
+          style={styles.headerLeftImg}
+          source={{
+            uri: AvatarPath,
+          }}
+        />
       </TouchableOpacity>
-    )
+    );
   }
 }
 
 const HeaderLeftCom = connect((state, props) => {
-  return Object.assign({}, {AvatarPath: state.UserInfo.AvatarPath}, props);
-})(HeaderLeft)
+  return Object.assign({}, { AvatarPath: state.UserInfo.AvatarPath }, props);
+})(HeaderLeft);
 
 class ErpIndex extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      headerTitle: (
-        <HeaderTitle title="ERP" textStyle={{color: '#fff'}}/>
-      ),
-      headerLeft: <HeaderLeftCom navigation={navigation}/>,
-      headerRight: <HeaderRight />
+      headerTitle: <HeaderTitle title="ERP" textStyle={{ color: "#fff" }} />,
+      headerLeft: <HeaderLeftCom navigation={navigation} />,
+      headerRight: <HeaderRight />,
     };
   };
   toErpList(name) {
-    const {navigation} = this.props;
-    navigation.push('ErpList', {name});
+    const { navigation } = this.props;
+    navigation.push("ErpList", { name });
   }
-  
+
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.row} activeOpacity={0.8} onPress={() => {this.toErpList('StkStock')}}>
+        <TouchableOpacity
+          style={styles.row}
+          activeOpacity={0.8}
+          onPress={() => {
+            this.toErpList("StkStock");
+          }}
+        >
           <Feather style={styles.icon} name="box" size={20} />
           <Text style={styles.title}>库存</Text>
-          <AntDesign style={styles.icon} name="right" size={20}/>
+          <AntDesign style={styles.icon} name="right" size={20} />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.row} activeOpacity={0.8} onPress={() => {this.toErpList('StkInRecord')}}>
+        <TouchableOpacity
+          style={styles.row}
+          activeOpacity={0.8}
+          onPress={() => {
+            this.toErpList("StkInRecord");
+          }}
+        >
           <Feather style={styles.icon} name="box" size={20} />
           {/* <Text style={{fontFamily:'iconfont', fontSize: 20}}>&#xe6ee;</Text> */}
-          
+
           <Text style={styles.title}>入库</Text>
-          <AntDesign style={styles.icon} name="right" size={20}/>
+          <AntDesign style={styles.icon} name="right" size={20} />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.row} activeOpacity={0.8} onPress={() => {this.toErpList('StkOutByModel')}}>
+        <TouchableOpacity
+          style={styles.row}
+          activeOpacity={0.8}
+          onPress={() => {
+            this.toErpList("StkOutByModel");
+          }}
+        >
           <Feather style={styles.icon} name="box" size={20} />
           <Text style={styles.title}>出库</Text>
-          <AntDesign style={styles.icon} name="right" size={20}/>
+          <AntDesign style={styles.icon} name="right" size={20} />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.row} activeOpacity={0.8} onPress={() => {this.toErpList('StkInquireRecord')}}>
+        <TouchableOpacity
+          style={styles.row}
+          activeOpacity={0.8}
+          onPress={() => {
+            this.toErpList("StkInquireRecord");
+          }}
+        >
           <Feather style={styles.icon} name="box" size={20} />
           <Text style={styles.title}>询价</Text>
-          <AntDesign style={styles.icon} name="right" size={20}/>
+          <AntDesign style={styles.icon} name="right" size={20} />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.row} activeOpacity={0.8} onPress={() => {this.toErpList('HisofferpriceByModel')}}>
+        <TouchableOpacity
+          style={styles.row}
+          activeOpacity={0.8}
+          onPress={() => {
+            this.toErpList("HisofferpriceByModel");
+          }}
+        >
           <Feather style={styles.icon} name="box" size={20} />
           <Text style={styles.title}>报价</Text>
-          <AntDesign style={styles.icon} name="right" size={20}/>
+          <AntDesign style={styles.icon} name="right" size={20} />
         </TouchableOpacity>
       </View>
-    )
+    );
   }
   componentWillMount() {
-    const {navigation} = this.props;
+    const { navigation } = this.props;
     // this.willBlurListener = navigation.addListener('willBlur', this.willBlurHandler);
-    this.willFocusListener = navigation.addListener('willFocus', this.willFocusHandler);
+    this.willFocusListener = navigation.addListener(
+      "willFocus",
+      this.willFocusHandler
+    );
   }
   componentWillUnmount() {
     // this.willBlurListener.remove();
     this.willFocusListener.remove();
   }
-  // willBlurHandler = () =>{
-  //     const {SetIsTabBarShow} = this.props;
-  //     SetIsTabBarShow(false);
-  // }
   willFocusHandler = () => {
-      const {SetIsTabBarShow} = this.props;
-      SetIsTabBarShow(true);
-
-  }
+    const { SetIsTabBarShow } = this.props;
+    SetIsTabBarShow(true);
+  };
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff",
   },
   headerLeftImg: {
-    width: 40, 
+    width: 40,
     height: 40,
     borderRadius: 20,
-    marginLeft: 10
+    marginLeft: 10,
   },
   row: {
     borderBottomWidth: 1,
-    borderColor: '#E6E6E6',
+    borderColor: "#E6E6E6",
     height: 50,
-    flexDirection: 'row',
-    alignItems:'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     // paddingLeft: 20,
     // paddingRight: 20,
   },
@@ -127,28 +148,28 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 20,
     // textAlign: 'left',
-  },  
+  },
   icon: {
     width: 50,
-    textAlign: 'center',
-    color: '#999',
-    fontWeight: 'bold',
-  }
+    textAlign: "center",
+    color: "#999",
+    fontWeight: "bold",
+  },
 });
 
 const mapStateToProps = (state, props) => {
   return Object.assign({}, props);
-}
-const mapDispatchToProps = (dispatch) => {
+};
+const mapDispatchToProps = dispatch => {
   return {
-      SetIsTabBarShow: (IsTabBarShow) => {
+    SetIsTabBarShow: IsTabBarShow => {
       return dispatch({
-          type: 'SetIsTabBarShow',
-          IsTabBarShow
-      })
-      }
-  }
-}
+        type: "SetIsTabBarShow",
+        IsTabBarShow,
+      });
+    },
+  };
+};
 
 export default connect(
   mapStateToProps,
--- src/pages/IndexPages/BomIndex.js
+++ src/pages/IndexPages/BomIndex.js
@@ -1,158 +1,163 @@
-import React, {Component} from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import {connect} from 'react-redux';
-import {PR} from '@src/utils/system'
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+/* @flow */
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-import {
-  HeaderTitle,
-  HeaderRight,
-} from '@components';
+// type testObj = {
+//   a: string,
+//   b: ?number,
+//   c?: booleal,
+// };
+
+import { HeaderTitle, HeaderRight } from "@components";
 class HeaderLeft extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      AvatarPath: 'http://bom-ai-read.oss-cn-shenzhen.aliyuncs.com/makesureFile/JCNEeK_1540978339916.jpg',
-    }
+      AvatarPath:
+        "http://bom-ai-read.oss-cn-shenzhen.aliyuncs.com/makesureFile/JCNEeK_1540978339916.jpg",
+    };
   }
   onPress = () => {
-    const {navigation} = this.props;
+    const { navigation } = this.props;
     navigation.openDrawer();
-  }
+  };
   componentWillMount() {
     Cloud.$getStorage(Cloud.$CONFIG.AvatarPath).then(data => {
       if (data) {
         this.setState({
-          AvatarPath: `https:${data}`
-        })
+          AvatarPath: `https:${data}`,
+        });
       }
-    })
+    });
   }
   render() {
     const { AvatarPath } = this.state;
     return (
-      <TouchableOpacity
-      activeOpacity={0.8}
-      onPress={ this.onPress }>
-        <Image 
-        style={styles.headerLeftImg}
-        source={{
-          uri: AvatarPath
-        }} />
+      <TouchableOpacity activeOpacity={0.8} onPress={this.onPress}>
+        <Image
+          style={styles.headerLeftImg}
+          source={{
+            uri: AvatarPath,
+          }}
+        />
       </TouchableOpacity>
-    )
+    );
   }
 }
 
 const HeaderLeftCom = connect((state, props) => {
-  return Object.assign({}, {AvatarPath: state.UserInfo.AvatarPath}, props);
-})(HeaderLeft)
+  return Object.assign({}, { AvatarPath: state.UserInfo.AvatarPath }, props);
+})(HeaderLeft);
 
 class Bom extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
       // title: '首页 | BomAi',
       headerTitle: (
-        <HeaderTitle title="首页 | BomAi" textStyle={{color: '#fff'}}/>
+        <HeaderTitle title="首页 | BomAi" textStyle={{ color: "#fff" }} />
       ),
-      headerLeft: <HeaderLeftCom navigation={navigation}/>,
-      headerRight: <HeaderRight />
+      headerLeft: <HeaderLeftCom navigation={navigation} />,
+      headerRight: <HeaderRight />,
     };
   };
   toSearchPage = () => {
-    const {navigation} = this.props;
-    navigation.navigate('SearchPage');
-  }
+    const { navigation } = this.props;
+    navigation.navigate("SearchPage");
+  };
   render() {
     return (
       <View style={styles.container}>
-          <View style={styles.ImgBox}>
-            <Image 
+        <View style={styles.ImgBox}>
+          <Image
             style={styles.HomeLogo}
             source={{
-              uri: 'http://bom-ai-read.oss-cn-shenzhen.aliyuncs.com/makesureFile/eBPk33_1539748592007.png',
-              cache: 'force-cache'
-              }} />
-          </View>
-          <TouchableOpacity 
-            style={styles.SearchBox} 
-            onPress={this.toSearchPage}
-            activeOpacity={1}>
-              <FontAwesome
-                name={'search'}
-                size={ 24 }
-                style={styles.FontAwesome}/>
-          </TouchableOpacity>
+              uri:
+                "http://bom-ai-read.oss-cn-shenzhen.aliyuncs.com/makesureFile/eBPk33_1539748592007.png",
+              cache: "force-cache",
+            }}
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.SearchBox}
+          onPress={this.toSearchPage}
+          activeOpacity={1}
+        >
+          <FontAwesome name={"search"} size={24} style={styles.FontAwesome} />
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
   componentWillMount() {
-    const {navigation} = this.props;
-    this.willFocusListener = navigation.addListener('willFocus', this.willFocusHandler);
+    const { navigation } = this.props;
+    this.willFocusListener = navigation.addListener(
+      "willFocus",
+      this.willFocusHandler
+    );
   }
   componentWillUnmount() {
     this.willFocusListener.remove();
   }
   willFocusHandler = () => {
-      const {SetIsTabBarShow} = this.props;
-      SetIsTabBarShow(true);
-
-  }
+    const { SetIsTabBarShow } = this.props;
+    SetIsTabBarShow(true);
+  };
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
   headerLeftImg: {
-    width: 40, 
+    width: 40,
     height: 40,
     borderRadius: 20,
-    marginLeft: 10
+    marginLeft: 10,
   },
   ImgBox: {
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center",
     height: 40,
-    paddingBottom: 30
+    paddingBottom: 30,
   },
   HomeLogo: {
-    width: 240, 
+    width: 240,
     height: 40,
   },
   SearchBox: {
     paddingLeft: 40,
     borderRadius: 10,
     fontSize: 16,
-    color: '#333333',
+    color: "#333333",
     borderWidth: 1,
-    borderColor: '#ee7700',
+    borderColor: "#ee7700",
     height: 40,
     marginLeft: 30,
     marginRight: 30,
   },
   FontAwesome: {
-    position: 'absolute',
+    position: "absolute",
     left: 10,
     top: 8,
-    color: '#999'
-  }
+    color: "#999",
+  },
 });
 
 const mapStateToProps = (state, props) => {
   return props;
-}
-const mapDispatchToProps = (dispatch) => {
+};
+const mapDispatchToProps = dispatch => {
   return {
-      SetIsTabBarShow: (IsTabBarShow) => {
+    SetIsTabBarShow: IsTabBarShow => {
       return dispatch({
-          type: 'SetIsTabBarShow',
-          IsTabBarShow
-      })
-      }
-  }
-}
+        type: "SetIsTabBarShow",
+        IsTabBarShow,
+      });
+    },
+  };
+};
 
 export default connect(
   mapStateToProps,
