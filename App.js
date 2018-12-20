5731b83b4037506cba5e339fde74006d046b20c7
vito <vitozhangyf@outlook.com>
2018-12-20 14:49:36 +0800

    关闭热更新提示

--- App.js
+++ App.js
@@ -42,24 +42,24 @@ class App extends Component<Props> {
       //IMMEDIATE 马上更新
       installMode: codePush.InstallMode.ON_NEXT_RESUME,
       //对话框
-      updateDialog: {
-        //是否显示更新描述
-        appendReleaseDescription: true,
-        //更新描述的前缀。 默认为"Description" 更新内容
-        descriptionPrefix: "",
-        //强制更新按钮文字，默认为continue
-        mandatoryContinueButtonLabel: "立即更新",
-        //强制更新时的信息. 默认为"An update is available that must be installed."
-        mandatoryUpdateMessage: "",
-        //非强制更新时，按钮文字,默认为"ignore"
-        optionalIgnoreButtonLabel: "稍后",
-        //非强制更新时，确认按钮文字. 默认为"Install"
-        optionalInstallButtonLabel: "后台更新",
-        //非强制更新时，检查到更新的消息文本
-        optionalUpdateMessage: "",
-        //Alert窗口的标题
-        title: "有新版本了，是否更新？",
-      },
+      // updateDialog: {
+      //   //是否显示更新描述
+      //   appendReleaseDescription: true,
+      //   //更新描述的前缀。 默认为"Description" 更新内容
+      //   descriptionPrefix: "",
+      //   //强制更新按钮文字，默认为continue
+      //   mandatoryContinueButtonLabel: "立即更新",
+      //   //强制更新时的信息. 默认为"An update is available that must be installed."
+      //   mandatoryUpdateMessage: "",
+      //   //非强制更新时，按钮文字,默认为"ignore"
+      //   optionalIgnoreButtonLabel: "稍后",
+      //   //非强制更新时，确认按钮文字. 默认为"Install"
+      //   optionalInstallButtonLabel: "后台更新",
+      //   //非强制更新时，检查到更新的消息文本
+      //   optionalUpdateMessage: "",
+      //   //Alert窗口的标题
+      //   title: "有新版本了，是否更新？",
+      // },
     });
   }
   componentWillMount() {
