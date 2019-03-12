ce36560b00126bb8bce10e7b37b97fcdca52f7c3
vito <vitozhangyf@outlook.com>
2019-03-12 10:53:46 +0800

    code-push测试环境

--- android/app/src/main/java/com/znlicloud/MainApplication.java
+++ android/app/src/main/java/com/znlicloud/MainApplication.java
@@ -52,9 +52,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(), new InstallApkPackager(), new PickerViewPackage(),
           new WeChatPackage(), new MPAndroidChartPackage(), new RNDeviceInfo(), new SplashScreenReactPackage(),
-          new RNFSPackage(), new CodePush(CODEPUSH_KEY_PRODUCTIO, getApplicationContext(), BuildConfig.DEBUG),
-          // new CodePush(CODEPUSH_KEY_STAGING, getApplicationContext(),
-          // BuildConfig.DEBUG), new VectorIconsPackage(),
+          // new RNFSPackage(), new CodePush(CODEPUSH_KEY_PRODUCTIO,
+          // getApplicationContext(), BuildConfig.DEBUG),
+          new CodePush(CODEPUSH_KEY_STAGING, getApplicationContext(), BuildConfig.DEBUG), new VectorIconsPackage(),
           new JPushPackage(SHUTDOWN_TOAST, SHUTDOWN_LOG));
     }
 
